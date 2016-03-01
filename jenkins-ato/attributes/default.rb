7dbd303f7736f68483a5513994208e2170c289f8
Artur K <Artur_Kiryiak@epam.com>
2016-03-01 19:07:33 +0300

    add Rakefile

--- jenkins-ato/attributes/default.rb
+++ jenkins-ato/attributes/default.rb
@@ -1,3 +1,3 @@
-default['java']['install_flavour']='oracle'
-default['java']['jdk_version']='7'
-default['java']['oracle']['accept_oracle_download_terms']=true
+default['java']['install_flavour'] = 'oracle'
+default['java']['jdk_version'] = '7'
+default['java']['oracle']['accept_oracle_download_terms'] = true
--- jenkins-ato/test/integration/default/serverspec/default_spec.rb
+++ jenkins-ato/test/integration/default/serverspec/default_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe "Java" do
+describe 'Java' do
   describe command('java -version') do
     its(:stderr) { should match /java version "1.7.0_95"/ }
   end
 end
 
-describe "Jenkins" do
+describe 'Jenkins' do
   describe package('jenkins') do
-    it { should be_installed}
+    it { should be_installed }
   end
 end
